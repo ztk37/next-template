9c1fe5853eecad572e88d8622875572b7020df40
ztk37 <ztk37.contact@gmail.com>
2023-09-14 18:01:24 +0200

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import "./global.css";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
